33d3f25858a92ba82476f4acc6fdb1bbf280f8f3
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-06-07 16:03:15 -0300

    :sparkles: feat: Resolvido o exercicio 06

---
+++ src/Exercicio06.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// Escreva um algoritmo que leia uma sequência de números do usuário e realize a soma desses números.
+// Encerre a execução quando um número negativo for digitado.
+public class Exercicio06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Integer> numeros = new ArrayList<>();
+
+        int soma = 0;
+        boolean condicao = true;
+        do{
+            System.out.println("Digite um número: ");
+            int numero = sc.nextInt();
+            if (numero<0){
+                break;
+            }
+            numeros.add(numero);
+            soma+=numero;
+        } while (true);
+
+        System.out.println("Os números digitados foram: " + numeros);
+        System.out.println("A soma dos números é: " + soma);
+
+
+    }
+}
