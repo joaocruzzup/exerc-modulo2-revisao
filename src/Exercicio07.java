fb6043359a77116cb34b6df09aa7765f31552dee
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-06-07 16:08:32 -0300

    :sparkles: feat: Resolvido o exercicio 07

---
+++ src/Exercicio07.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+// Leia o nome do usuário e um número N, após isso escreva o nome dele na tela N vezes.
+public class Exercicio07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String nomeUsuario = sc.nextLine();
+
+        System.out.println("Digite um número positivo: ");
+        int numero = sc.nextInt();
+
+        while (numero <=0){
+            System.out.println("Digite um número positivo: ");
+            numero = sc.nextInt();
+        }
+
+        for (int i = 0; i < numero; i++){
+            System.out.printf("%s %n", nomeUsuario);
+        }
+    }
+}
